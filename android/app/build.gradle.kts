5ac0645f202a25970412af0dbcaf0fa0ad5ac922
virzst <sayaganten233444@gmail.com>
2026-05-10 05:17:06 +0700

    Change application namespace and ID to 'com.voyre.app'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 android {
-    namespace = "com.prikitiww.app"
+    namespace = "com.voyre.app"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -31,7 +31,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.prikitiww.app"
+        applicationId = "com.voyre.app"
         minSdk = 23                         // ✅ ganti di sini
         targetSdk = 36
         versionCode = flutter.versionCode
